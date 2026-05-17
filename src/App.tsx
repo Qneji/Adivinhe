abab3a43ca3bd3260e4e4569f6afec918cdfe2cf
kenny <kennedy_ken99@hotmail.com>
2026-05-17 13:59:42 -0300

    Deixando o header de maneira dinâmica

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { Header } from "./components/Header"
 import styles from "./app.module.css"
 
 export default function  App() {
+  
+  function handleRestartGame() {
+    alert("O jogo foi reiniciado!")
+  }
+  
   return (
     <div className={styles.container}>
       <main>
-        < Header />
+        < Header current={5} max={10} onRestart={handleRestartGame}/>
       </main>
     </div>
   )

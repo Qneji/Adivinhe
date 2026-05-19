89047294fc45db1f3bc4339915458aafa4af0528
kenny <kennedy_ken99@hotmail.com>
2026-05-19 17:06:02 -0300

    Usando useEffect e useState para lidar com as palavras sorteadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import styles from "./app.module.css"
+import { useEffect, useState } from "react"
+
+import { WORDS, type Challenge } from "./utils/words"
+
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
@@ -5,20 +10,35 @@ import { Input } from "./components/Input"
 import { Button } from "./components/Button"
 import { LettersUsed } from "./components/LettersUsed"
 
-import styles from "./app.module.css"
+export default function App() {
+  const [letter, setLetter] = useState("")
+  const [attempts, setAttempts] = useState(0)
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
 
-export default function  App() {
-  
   function handleRestartGame() {
     alert("O jogo foi reiniciado!")
   }
-  
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+
+    setChallenge(randomWord)
+
+    setAttempts(0)
+    setLetter("")
+  }
+
+  useEffect(() => {
+    startGame()
+  }, [])
+
   return (
     <div className={styles.container}>
       <main>
-        < Header current={5} max={10} onRestart={handleRestartGame}/>
+        < Header current={attempts} max={10} onRestart={handleRestartGame} />
 
-        < Tip tip="Uma das linguagens de programação mais usadas" />
+        <Tip tip="Uma das linguagens de programação mais usadas" />
 
         <div className={styles.word}>
           <Letter value="r" />
@@ -30,12 +50,11 @@ export default function  App() {
         <h4>Palpite</h4>
 
         <div className={styles.guess}>
-          < Input placeholder="?" autoFocus maxLength={1} />
-          < Button title="Confirmar" />
+          <Input placeholder="?" autoFocus maxLength={1} />
+          <Button title="Confirmar" />
         </div>
 
-        < LettersUsed />
-        
+        <LettersUsed />
       </main>
     </div>
   )

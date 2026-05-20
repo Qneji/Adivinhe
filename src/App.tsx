0d2b39214c34a1c9051f2fe4b23fa209c79bf444
kenny <kennedy_ken99@hotmail.com>
2026-05-20 11:13:31 -0300

    Criando função para letras utilizadas

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,27 @@ export default function App() {
     setLetter("")
   }
 
+  function handleConfirm(){
+    if (!challenge) {
+      return
+    }
+
+    if (!letter.trim()) {
+      return alert("Digite uma letra!")
+    }
+
+    const value = letter.toUpperCase()
+    const exist = lettersUsed.find((used) => used.value.toUpperCase() === value)
+
+    if(exist) {
+      return alert("Você já utilizou a letra " + value)
+    }
+
+    setLettersUsed((prevState) => [...prevState, {value, correct:false}])
+
+    setLetter("")
+  }
+
   useEffect(() => {
     startGame()
   }, [])
@@ -54,8 +75,13 @@ export default function App() {
         <h4>Palpite</h4>
 
         <div className={styles.guess}>
-          <Input placeholder="?" autoFocus maxLength={1} />
-          <Button title="Confirmar" />
+          <Input 
+            placeholder="?" 
+            autoFocus 
+            maxLength={1} 
+            value={letter}
+            onChange={(e) => setLetter(e.target.value)}/>
+          <Button title="Confirmar" onClick={handleConfirm}/>
         </div>
 
         <LettersUsed data={lettersUsed}/>
